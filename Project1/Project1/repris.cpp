5e4262541141e53c150ae00c2f0da34cf1a9afc1
user <user@DESKTOP-9MJP4FH>
2025-12-18 14:51:11 +0300

    1

--- Project1/Project1/repris.cpp
+++ Project1/Project1/repris.cpp
@@ -7,21 +7,3 @@ void main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	printf("Винковский\n");
-	printf("Винковский\n");
-	printf("Максим\n");
-	printf("Максим\n");
-	printf("Максим\n");
-	printf("Сергеевич\n");
-	printf("Сергеевич\n");
-	printf("Сергеевич\n");
-	printf("\n");
-	printf("Группа: ИСЭбд-11-2025\n");
-	printf("Группа: ИСЭбд-11-2025\n");
-	printf("\n");
-	printf("111");
-	printf("Дата: 03.09.\n");
-	printf("Дата: 03.09.\n");
-	printf("Предмет: Основы алгоритмизации и программирования\n");
-	printf("Лабораторная работа №1\n");
-}
